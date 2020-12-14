ec700ca07c611c7f2ce1867c70af3d1732466502
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-12-14 05:53:35 -0300

    criando arquivo de estilo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import CompPadrao, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
 
 export default () => {
     return (
-        <View>
+        <View style={style.App}>
             <CompPadrao />
             <Comp1/>
             <Comp2/>
@@ -14,3 +14,11 @@ export default () => {
         </View>
     );
 }
+
+const style = StyleSheet.create({
+    App: {
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
