ac6e8e74bcb5c5a756465ba1c901b709096ecd4e
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-12-16 05:37:42 -0300

    componente par impar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 
-import ContadorV2 from './components/contador/ContadorV2';
+import ParImpar from './components/PaiImpar';
+// import Diferenciar from './components/Diferenciar';
+// import ContadorV2 from './components/contador/ContadorV2';
 // import Pai from './components/indireta/Pai';
 // import Pai from './components/direta/Pai';
 // import Contador from './components/Contador';
@@ -14,9 +16,11 @@ import ContadorV2 from './components/contador/ContadorV2';
 
 export default () => {
     return (
-        <View style={style.App}>
-            <ContadorV2 />
+        <SafeAreaView style={style.App}>
+            <ParImpar num={6}/>
             {/*
+            <Diferenciar />
+            <ContadorV2 />
             <Pai />
             <Pai />
             <Contador inicial={100} passo={13}/>
@@ -30,7 +34,7 @@ export default () => {
             <Comp1/>
             <Comp2/>
             <Primeiro/> */}
-        </View>
+        </SafeAreaView>
     );
 }
 
